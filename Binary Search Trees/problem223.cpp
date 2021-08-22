e38d34b6c01126200ae732f537108022a568bd40
Prakhar Srivastava <sriprakhar2@gmail.com>
2021-08-22 08:29:01 +0530

    Add files via upload

---
+++ Binary Search Trees/problem223.cpp
@@ -0,0 +1,30 @@
+void inorder(Node* root, vector<Node*> &nodes)
+{
+    if (root==NULL)
+        return;
+    inorder(root->left, nodes);
+    nodes.push_back(root);
+    inorder(root->right, nodes);
+}
+ 
+/* Recursive function to construct binary tree */
+Node* buildTreeUtil(vector<Node*> &nodes, int start,
+                   int end)
+{
+    if (start > end)
+        return NULL;
+    int mid = (start + end)/2;
+    Node *root = nodes[mid];
+    root->left  = buildTreeUtil(nodes, start, mid-1);
+    root->right = buildTreeUtil(nodes, mid+1, end);
+ 
+    return root;
+}
+Node* buildTree(Node* root)
+{
+    vector<Node *> nodes;
+    inorder(root, nodes);
+
+    int n = nodes.size();
+    return buildTreeUtil(nodes, 0, n-1);
+}
